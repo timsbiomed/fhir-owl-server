6f0ef34abb48f6369175d2da2c94579af4aaca5b
jiaola <djiao@jhu.edu>
2019-09-18 12:20:13 -0400

    The simplest FHIR rest server

---
+++ fhir-owl-server/src/main/java/hot/fhirowl/server/FhirOwlServlet.java
@@ -0,0 +1,34 @@
+package hot.fhirowl.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.narrative.DefaultThymeleafNarrativeGenerator;
+import ca.uhn.fhir.narrative.INarrativeGenerator;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.server.interceptor.ResponseHighlighterInterceptor;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = {"/fhirowl/*"}, displayName = "FHIR OWL Server")
+public class FhirOwlServlet extends RestfulServer {
+    private static final long serialVersionUID = 1L;
+
+    public FhirOwlServlet() {
+        super(FhirContext.forR4());
+    }
+
+    @Override
+    public void initialize() {
+        List<IResourceProvider> providers = new ArrayList<>();
+        providers.add(new OwlCodeSystemResourceProvider());
+        setResourceProviders(providers);
+
+        INarrativeGenerator narrativeGen = new DefaultThymeleafNarrativeGenerator();
+        getFhirContext().setNarrativeGenerator(narrativeGen);
+
+        registerInterceptor(new ResponseHighlighterInterceptor());
+    }
+}
---
+++ fhir-owl-server/src/main/java/hot/fhirowl/server/OwlCodeSystemResourceProvider.java
@@ -0,0 +1,37 @@
+package hot.fhirowl.server;
+
+import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.Read;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.IdType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OwlCodeSystemResourceProvider implements IResourceProvider {
+    private Map<String, CodeSystem> codeSystems = new HashMap<>();
+
+    public OwlCodeSystemResourceProvider() {
+        CodeSystem cs = new CodeSystem();
+        cs.setId("1");
+        cs.addIdentifier().setSystem("").setValue("");
+        codeSystems.put("1", cs);
+    }
+
+    @Override
+    public Class<? extends IBaseResource> getResourceType() {
+        return CodeSystem.class;
+    }
+
+    @Read
+    public CodeSystem read(@IdParam IdType theId) {
+        CodeSystem cs = codeSystems.get(theId.getIdPart());
+        if (cs == null) {
+            throw new ResourceNotFoundException(theId);
+        }
+        return cs;
+    }
+}
