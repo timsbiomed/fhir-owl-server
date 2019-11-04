b710d1a74c0dc8be7e65cb0a5ee6ea9b00a69702
jiaola <djiao@jhu.edu>
2019-11-04 08:18:11 -0500

    Implements an abstract Neo4jResourceProvider

---
+++ fhir-owl-server/src/main/java/hot/fhir/owl/server/Neo4jResourceProvider.java
@@ -0,0 +1,25 @@
+package hot.fhir.owl.server;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+
+public abstract class Neo4jResourceProvider implements IResourceProvider, AutoCloseable {
+    protected final Driver driver;
+    protected FhirContext fhirContext;
+
+    public Neo4jResourceProvider(FhirContext fhirContext) {
+        this.fhirContext = fhirContext;
+        String url = System.getenv("NEO4J_URL");
+        String username = System.getenv("NEO4J_USERNAME");
+        String password = System.getenv("NEO4J_PASSWORD");
+        this.driver = GraphDatabase.driver(url, AuthTokens.basic(username, password));
+    }
+
+    @Override
+    public void close() throws Exception {
+        driver.close();
+    }
+}
