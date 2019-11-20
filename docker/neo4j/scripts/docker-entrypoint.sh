b1b599b7ab65338596e887beeb6294f79a183c41
jiaola <djiao@jhu.edu>
2019-11-20 12:16:58 -0500

    Start and stop neo4j so that data folder is created before data load

--- docker/neo4j/scripts/docker-entrypoint.sh
+++ docker/neo4j/scripts/docker-entrypoint.sh
@@ -407,6 +407,7 @@ if [ "${cmd}" == "dump-config" ]; then
     exit 0
 fi
 
+neo4j start; sleep 5; neo4j stop
 curl -L --output /opt/neo4j-cdmh.dump https://jh.box.com/shared/static/0olbvin16phw0kl96fmmmv1l4f2y6ylr.dump
 /var/lib/neo4j/bin/neo4j-admin load --database=graph.db --from=/opt/neo4j-cdmh.dump --force
 exec /var/lib/neo4j/bin/neo4j console
